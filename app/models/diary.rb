07c3802aceb0d323a61fe483b29361979924cb02
Lea Hayoun <hayounlea@gmail.com>
2025-12-10 15:36:25 +0100

    implement active text overview for diaries

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,6 +1,8 @@
 class Diary < ApplicationRecord
   belongs_to :user
 
-  # has_rich_text :content
+  has_rich_text :content # pas de colonne content dans table diaries, mais c'est normal
+
+  has_many_attached :documents
 
 end
